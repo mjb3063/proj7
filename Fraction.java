066dd6dd5cad49f4451b94511fdecf1b24659580
mjb3063 <mjb3063@ksu.edu>
2014-10-31 21:27:41 -0500

    fixed the README

---
+++ Fraction.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+
+public class Fraction {
+
+	Scanner keyboard = new Scanner (system.in);
+
+	int numerator;
+	int denominator;
+	
+	public fraction (int n, int d){
+		numerator = n;
+		denominator = d;
+	} // closes fraction 2 variable
+
+
+	public fraction (int n) {
+		numerator = n;
+		deominator = 1;
+	} // closes fraction 1 variable 
+
+
+	public fraction () {
+		numerator = 1;
+		denominator = 1;
+	} // closes fraction 0 variable
+
+
+	public String toString () {
+		return
+	} // closes toString
+
+
+	public Boolean equals (Fraction f) {
+
+	} // closes Boolean
+
+
+	public Fraction plus (Fraction f) {
+
+
+	} // closes Plus
+
+
+	public Fraction minus (Fraction f) {
+
+
+	} // closes Minus
+
+
+	Public Fraction times (Fraction f) {
+
+
+	} // closes Times
+
+
+	public Fraction divide (Fraction f) {
+
+
+	} // closes divide
+
+
+	private void reduce (Fraction f) {
+
+
+	} // closes reduce
+
+
+} // closes class FractionObject
+
+
+
+
--- FractionApp.java
+++ FractionApp.java
@@ -1,8 +1,10 @@
 import java.util.*;
 
 
-public class FractionApp{
+public class FractionApp {
 	public static void main (String [] arg) {
 
+		Scanner keyboard = new Scanner (system.in);
+		
 	} // closes main
 } // closes app
